4d8ed18db43d3e4d515cb19b3351aa36bb571ef9
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-10-24 00:35:07 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -49,7 +49,7 @@ url = 'https://www.dropbox.com/s/ni9567tj2x2r5b6/ml_engineering_weapon_and_no.zi
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_trained_model():
-    st.write('Cache miss: load_trained_model(load_model("Models/ml_engineering_weapon_and_no")) ran')
+    st.write('Cache miss: load_trained_model() ran')
     model = load_model("Models/ml_engineering_weapon_and_no")
     return model
 
@@ -60,7 +60,6 @@ model = load_trained_model()
 
 """## Загружаем изображение из файла в StreamLit"""
 
-@st.cache(allow_output_mutation=True)
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
