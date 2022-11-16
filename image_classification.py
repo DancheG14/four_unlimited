c4ec924e33a4459e1caf08800b081af4748f926c
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-11-17 01:28:08 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -102,7 +102,7 @@ if result:
     
 marks = [5,4,3] 
 
-if img is not None:
+if x is not None:
     selected_mark = st.radio("Ваша оценка проекта:", marks)
     st.write("Оценка:", selected_mark)
     if selected_mark == 5:
