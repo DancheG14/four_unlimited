712a512f0c9a183f5f775a93fa81c55034e8edd9
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-11-17 02:00:56 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -105,10 +105,10 @@ if pred_x:
     st.write('Результаты распознавания: \n ',sub + ",  с вероятностью:  " + print_percent(x))
     st.markdown("""### Андрей Владимирович, ваша оценка:""")
     
-    selected_mark = st.radio("Ваша оценка проекта:", marks)
+    selected_mark = st.radio("Ваша оценка проекта:", marks, value=False)
     st.write("Оценка:", selected_mark)
     if selected_mark == 5:
-        st.write("Ура" )
+        st.write("Ура")
     elif selected_mark == 4:
         st.write("Ну ладно")
     else:
